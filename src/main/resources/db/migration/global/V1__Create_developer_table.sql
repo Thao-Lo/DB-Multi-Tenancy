07ada359d2ab68ab0475ff16e6b51e6241049ca9
Zavis <thezavis@gmail.com>
2025-03-06 02:49:43 +1100

    Flyway: Developer, Owner, Tenant alter tables, add fields: created by, updated by, reset_token, reset_token_expiry, set password is null for user to reset with their own password

--- src/main/resources/db/migration/global/V1__Create_developer_table.sql
+++ src/main/resources/db/migration/global/V1__Create_developer_table.sql
@@ -3,10 +3,14 @@ CREATE TABLE `db_navigation_global_multi_tenant`.`developers` (
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
   `role` ENUM('SUPER_ADMIN', 'ADMIN') NOT NULL DEFAULT 'ADMIN',
+  `created_by` VARCHAR(100) NOT NULL,
+  `updated_by` VARCHAR(100) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `reset_token` VARCHAR(100) NULL DEFAULT NULL,
+  `reset_token_expiry` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),  
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
  
