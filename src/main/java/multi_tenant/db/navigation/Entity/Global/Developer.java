201db0d87af00585e058aa05f22dcb1e8ec6fef6
Zavis <thezavis@gmail.com>
2025-03-07 21:41:30 +1100

    Entity: using BaseUser abstract class for user entities: developer, owner, and tenantUser. set default roles for users in both hibernate and flyway

--- src/main/java/multi_tenant/db/navigation/Entity/Global/Developer.java
+++ src/main/java/multi_tenant/db/navigation/Entity/Global/Developer.java
@@ -2,55 +2,26 @@ package multi_tenant.db.navigation.Entity.Global;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import multi_tenant.db.navigation.Entity.BaseUser;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "developers", schema = "db_navigation_global_multi_tenant")
-public class Developer {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@Column(name="first_name",nullable = false, unique = false, length = 45)
-	private String firstName;
-
-	@Column(name="last_name",nullable = false, unique = false, length = 45)
-	private String lastName;
-	
-	@Column(nullable = false, unique = true, length = 100)
-	private String email;
-
-	@Column(nullable = false, length = 255)
-	private String password;
+public class Developer extends BaseUser {
 
 	@Enumerated(EnumType.STRING)
 	@Column(name="role", nullable=false)
-	private Role role;
-
-	@Column(name = "created_at")
-	@CreationTimestamp
-	private LocalDateTime createdAt;
-	
-	@Column(name = "updated_at")
-	@UpdateTimestamp
-	private LocalDateTime updatedAt;
+	private Role role = Role.ADMIN;
 	
 	public enum Role {
 		SUPER_ADMIN, ADMIN
--- src/main/java/multi_tenant/db/navigation/Entity/Global/Owner.java
+++ src/main/java/multi_tenant/db/navigation/Entity/Global/Owner.java
@@ -6,9 +6,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
@@ -16,50 +13,23 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import multi_tenant.db.navigation.Entity.BaseUser;
 import multi_tenant.db.navigation.Enum.Status;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "owners", schema = "db_navigation_global_multi_tenant")
-public class Owner {
-
-	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long id;	
-	
-	@Column(name="first_name",nullable = false, unique = true, length = 45)
-	private String firstName;
-
-	@Column(name="last_name",nullable = false, unique = true, length = 45)
-	private String lastName;
-	
-	@Column(nullable = false, unique = true, length = 100)
-	private String email;
-	
-	@Column(nullable = false, length = 255)
-	private String password;
+public class Owner extends BaseUser{
 	
 	@Enumerated(EnumType.STRING)
 	@Column
-	private Status status;
-	
-	@Column(name = "created_at")
-	@CreationTimestamp
-	private LocalDateTime createdAt;
-	
-	@Column(name = "updated_at")
-	@UpdateTimestamp
-	private LocalDateTime updatedAt;
+	private Status status = Status.ACTIVE;	
 	
 	@OneToMany (mappedBy = "owner", cascade = CascadeType.ALL) //owner lowecase, field in Tenant
 	@JsonIgnore
