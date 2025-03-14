8cdc8a1ba88a8e83aaf300c4e912c2506c33f337
Zavis <thezavis@gmail.com>
2025-03-15 00:05:05 +1100

    Global Service: SubPlanDetailService create method with custom query to Fetch PlanDetail using Owner findTop..OrderBy..., method to update additional admin/tenant

--- src/main/java/multi_tenant/db/navigation/Service/PaymentService.java
+++ src/main/java/multi_tenant/db/navigation/Service/PaymentService.java
@@ -7,15 +7,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import org.springframework.transaction.annotation.Transactional; 
+import org.springframework.transaction.annotation.Transactional;
+
+import multi_tenant.db.navigation.DTO.AdditionalType;
+import multi_tenant.db.navigation.DTO.PlanAdditionalPaymentRequest;
 import multi_tenant.db.navigation.DTO.SubPlanRequest;
 import multi_tenant.db.navigation.Entity.Global.Owner;
 import multi_tenant.db.navigation.Entity.Global.Payment;
 import multi_tenant.db.navigation.Entity.Global.PaymentDetail;
+import multi_tenant.db.navigation.Entity.Global.ProrataDetail;
 import multi_tenant.db.navigation.Entity.Global.SubPlanDetail;
 import multi_tenant.db.navigation.Entity.Global.SubscriptionPlan;
+import multi_tenant.db.navigation.Exception.NoDataFoundException;
 import multi_tenant.db.navigation.Repository.Global.PaymentDetailRepository;
 import multi_tenant.db.navigation.Repository.Global.PaymentRepository;
+import multi_tenant.db.navigation.Repository.Global.ProrataDetailRepository;
 
 @Service
 public class PaymentService {
@@ -25,10 +31,28 @@ public class PaymentService {
 
 	@Autowired
 	private PaymentDetailService paymentDetailService;
-	
+
 	@Autowired
 	private PaymentDetailRepository paymentDetailRepository;
+
+	@Autowired
+	private ProrataDetailService prorataDetailService;
 	
+	@Autowired
+	private ProrataDetailRepository prorataDetailRepository;
+
+	public Payment getPaymentById(long id) {
+		return paymentRepository.findById(id)
+				.orElseThrow(() -> new NoDataFoundException("No payment found for this id: " + id));
+
+	}
+
+	public Payment getPaymentByPaymentIntentId(String id) {
+		return paymentRepository.findByPaymentIntentId(id)
+				.orElseThrow(() -> new NoDataFoundException("No payment found for this id: " + id));
+
+	}
+
 	@Transactional(transactionManager = "globalTransactionManager")
 	public void saveInitPayment(Owner owner, SubPlanDetail planDetail, BigDecimal amount, String paymentIntentId,
 			SubPlanRequest request, SubscriptionPlan plan) {
@@ -41,32 +65,79 @@ public class PaymentService {
 		payment.setPaymentStatus(Payment.PaymentStatus.PENDING);
 
 		payment = paymentRepository.save(payment);
-		
+
 		List<PaymentDetail> paymentDetails = new ArrayList<>();
 		// each payment break down to different type
-		paymentDetails.add(paymentDetailService.createPaymentDetail(payment, 1,  plan.getBaseCost(), 1, plan.getBaseCost()));
+		paymentDetails
+				.add(paymentDetailService.createPaymentDetail(payment, 1, plan.getBaseCost(), 1, plan.getBaseCost()));
 
 		if (request.getAdditionalAdminCount() > 0) {
 			BigDecimal additionalAdminAmount = plan.getAdditionalAmindFee()
 					.multiply(BigDecimal.valueOf(request.getAdditionalAdminCount()));
-			
-			paymentDetails.add(paymentDetailService.createPaymentDetail(
-					payment, 2, additionalAdminAmount,
+
+			paymentDetails.add(paymentDetailService.createPaymentDetail(payment, 2, additionalAdminAmount,
 					request.getAdditionalAdminCount(), plan.getAdditionalAmindFee()));
 
 		}
 		if (request.getAdditionalTenantCount() > 0) {
 			BigDecimal additionalTenantAmount = plan.getAdditionalTenantFee()
 					.multiply(BigDecimal.valueOf(request.getAdditionalTenantCount()));
-			
-			
-			paymentDetails.add(paymentDetailService.createPaymentDetail(
-					payment, 3, additionalTenantAmount,
+
+			paymentDetails.add(paymentDetailService.createPaymentDetail(payment, 3, additionalTenantAmount,
 					request.getAdditionalTenantCount(), plan.getAdditionalTenantFee()));
 		}
 		if (paymentDetails.isEmpty()) {
-		    throw new IllegalStateException("No payment details found to save.");
+			throw new IllegalStateException("No payment details found to save.");
 		}
 		paymentDetailRepository.saveAll(paymentDetails);
+	}
+
+	// DO MORE HERE
+	@Transactional(transactionManager = "globalTransactionManager")
+	public void savePlanAdditionalPayment(PlanAdditionalPaymentRequest request) {
+		// save to payment table
+		Payment payment = new Payment();
+		payment.setOwner(request.getOwner());
+		payment.setSubPlanDetail(request.getPlanDetail());
+		payment.setAmount(request.getAmount());
+		payment.setPaymentIntentId(request.getPaymentIntentId());
+		payment.setPaymentStatus(Payment.PaymentStatus.PENDING);
+
+		payment = paymentRepository.save(payment);
+
+		if (AdditionalType.ADMIN.name().equals(request.getAdditionalType())) {
+			prorataDetailRepository.save(
+					prorataDetailService.createProrataDetail(
+							payment, 2, request.getAmount(), request.getCount(), 
+							request.getPlan().getAdditionalAmindFee(), request.getRemainingDays()));
+			
+		}
+		if (AdditionalType.TENANT.name().equals(request.getAdditionalType())) {
+			prorataDetailRepository.save(
+					prorataDetailService.createProrataDetail(
+							payment, 3, request.getAmount(), request.getCount(), 
+							request.getPlan().getAdditionalTenantFee(), request.getRemainingDays()));
+		}		
+
+	}
+
+	@Transactional(transactionManager = "globalTransactionManager")
+	public void changePaymentStatusToSuccess(long paymentId) {
+		Payment payment = getPaymentById(paymentId);
+
+		payment.setPaymentStatus(Payment.PaymentStatus.SUCCESS);
+
+		paymentRepository.save(payment);
+	}
+
+	// for payment-retry
+	@Transactional(transactionManager = "globalTransactionManager")
+	public void changePaymentStatusToFailed(long paymentId, String newPaymentIntentId) {
+		Payment payment = getPaymentById(paymentId);
+
+		payment.setPaymentIntentId(newPaymentIntentId);
+		payment.setPaymentStatus(Payment.PaymentStatus.FAILED);
+
+		paymentRepository.save(payment);
 	}
 }
--- src/main/java/multi_tenant/db/navigation/Service/SubPlanDetailService.java
+++ src/main/java/multi_tenant/db/navigation/Service/SubPlanDetailService.java
@@ -1,9 +1,13 @@
 package multi_tenant.db.navigation.Service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import org.springframework.transaction.annotation.Transactional; 
+import org.springframework.transaction.annotation.Transactional;
+
+import multi_tenant.db.navigation.DTO.StripePaymentType;
 import multi_tenant.db.navigation.DTO.SubPlanRequest;
 import multi_tenant.db.navigation.Entity.Global.Owner;
 import multi_tenant.db.navigation.Entity.Global.SubPlanDetail;
@@ -17,25 +21,51 @@ public class SubPlanDetailService {
 	@Autowired
 	private SubPlanDetailRepository subPlanDetailRepository;
 	
-	@Autowired
-	private SubscriptionPlanService subPlanService;  
-	
+
 	public SubPlanDetail getSubPlanDetailById(long id) {
 		return subPlanDetailRepository.findById(id)
 				.orElseThrow(() -> new NoDataFoundException("No plan details found for this id: " + id));
 	}
 	
+	public SubPlanDetail getLatestPlanDetailByOwner(Owner owner) {
+		return subPlanDetailRepository.findTopByOwnerOrderBySubscriptionStartDesc(owner)
+				.orElseThrow(() -> new NoDataFoundException ("No Subscription Plan Detail found."));
+	}
+
+	// initial saving data to the table, Status: INACTIVE
 	@Transactional(transactionManager = "globalTransactionManager")
-	public SubPlanDetail saveInitialPlanDetail(SubPlanRequest request, Owner owner, SubscriptionPlan plan) {		
-		SubPlanDetail planDetail = new SubPlanDetail();		
+	public SubPlanDetail saveInitialPlanDetail(SubPlanRequest request, Owner owner, SubscriptionPlan plan) {
+		SubPlanDetail planDetail = new SubPlanDetail();
 		planDetail.setOwner(owner);
 		planDetail.setSubscriptionPlan(plan);
-		planDetail.setAdditionalTenantCount(request.getAdditionalTenantCount());	
+		planDetail.setAdditionalTenantCount(request.getAdditionalTenantCount());
 		planDetail.setMaxTenant(plan.getTenantLimit() + request.getAdditionalTenantCount());
 		planDetail.setAdditionalAdminCount(request.getAdditionalAdminCount());
 		planDetail.setStatus(SubPlanDetail.PlanDetailStatus.INACTIVE);
-			
-		return subPlanDetailRepository.save(planDetail);		
+
+		return subPlanDetailRepository.save(planDetail);
 	}
-	
+
+	// change from INACTIVE to ACTIVE
+	public void activateSubscriptionPlan(long planDetailId) {
+		SubPlanDetail planDetail = getSubPlanDetailById(planDetailId);
+
+		// update plan from INACTIVE to ACTIVE when payment success
+		planDetail.setStatus(SubPlanDetail.PlanDetailStatus.ACTIVE);
+		planDetail.setSubscriptionStart(LocalDateTime.now());
+
+		subPlanDetailRepository.save(planDetail);
+	}
+
+	// change admin or tenant count
+	public void updateAdditionalCount(long planDetailId, StripePaymentType paymentType, int count) {
+		SubPlanDetail planDetail = getSubPlanDetailById(planDetailId);
+		//ENUM is a SINGLETON, 1 object 1 reference
+		if (paymentType == StripePaymentType.ADDITIONAL_ADMIN) {
+			planDetail.setAdditionalAdminCount(planDetail.getAdditionalAdminCount() + count);
+		}
+		if (paymentType == StripePaymentType.ADDITIONAL_TENANT) {
+			planDetail.setAdditionalTenantCount(planDetail.getAdditionalTenantCount() + count);		}
+	}
+
 }
