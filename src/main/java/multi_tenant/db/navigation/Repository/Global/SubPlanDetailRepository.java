749bd74ef4a2d3df7d611535cb6a3783d89badc7
Zavis <thezavis@gmail.com>
2025-03-16 22:12:16 +1100

    Global Repository Payment and SubPlanDetail respo

--- src/main/java/multi_tenant/db/navigation/Repository/Global/SubPlanDetailRepository.java
+++ src/main/java/multi_tenant/db/navigation/Repository/Global/SubPlanDetailRepository.java
@@ -1,9 +1,13 @@
 package multi_tenant.db.navigation.Repository.Global;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import multi_tenant.db.navigation.Entity.Global.Owner;
 import multi_tenant.db.navigation.Entity.Global.SubPlanDetail;
 
 public interface SubPlanDetailRepository extends JpaRepository<SubPlanDetail, Long>{
-
+	//findTopBy...OrderBy...Desc/Asc
+	Optional<SubPlanDetail> findTopByOwnerOrderBySubscriptionStartDesc(Owner owner);
 }
