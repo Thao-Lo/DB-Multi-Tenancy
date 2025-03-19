ca1a512d1a534d0526bc94c3267f8bf9d82f71bd
Zavis <thezavis@gmail.com>
2025-03-19 21:07:15 +1100

    General - Application enable Schedule

--- src/main/java/multi_tenant/db/navigation/Application.java
+++ src/main/java/multi_tenant/db/navigation/Application.java
@@ -2,8 +2,10 @@ package multi_tenant.db.navigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
