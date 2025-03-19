ab1d8fb9303d74fff6979949318edc699e4d06df
Zavis <thezavis@gmail.com>
2025-03-19 21:04:42 +1100

    Local - Entity: TenantAdminLimit

---
+++ src/main/java/multi_tenant/db/navigation/Entity/Tenant/TenantAdminLimit.java
@@ -0,0 +1,44 @@
+package multi_tenant.db.navigation.Entity.Tenant;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tenant_admin_limit")
+public class TenantAdminLimit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name = "id")	
+	private int id;
+	
+	@Column(name = "database_name", nullable = false, length = 100)
+	private String databaseName;
+
+	@Column(name = "current_admin_count", nullable = false, length = 100)
+	private String currentAdminCount;
+
+	@Column(name = "max_admin_count", nullable = false, length = 100)
+	private String maxAdminCount;
+
+	@Column(name = "updated_by")
+	private String updatedBy;
+
+	@Column(name = "created_at")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+}
